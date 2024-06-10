43b7b531d37428f6d466a4731f7be1788f5187b8
Robinhood <matanaluf0@yahoo.com>
2024-06-11 01:23:09 +0300

    Use env variables and create Procfile.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date
-
+import os
 import flask
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
@@ -44,7 +44,8 @@ def only_commenter(function):
     return check
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+# 8BYkEfBA6O6donzWlSihBXox7C0sKR6b
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 gravatar = Gravatar(app,
@@ -64,7 +65,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -258,4 +259,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
